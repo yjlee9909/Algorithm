d2e4b7650d09aa34ccd3ce5b922e1a1f1ef3e1b9
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-10-07 23:22:22 +0900

    [unrated] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    var answer = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        answer += parseInt(numbers[i]);
+    }
+    return (answer/numbers.length);
+}
