ec8f11e261617487ea786bfd352f13cdb00be419
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-10-14 20:39:35 +0900

    [unrated] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
@@ -1,3 +1,7 @@
 function solution(n, k) {
-    return (n * 12000) + 2000*(k-parseInt(n/10));
+    if (n < 10) {
+        return (n * 12000) + (k * 2000);
+    } else {
+        return (n * 12000) + 2000*(k-parseInt(n/10));
+    }  
 }
