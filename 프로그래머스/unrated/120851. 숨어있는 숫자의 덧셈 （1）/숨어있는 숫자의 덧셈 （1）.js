bfe3194076ece440bebb8b89398c9e156295c582
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-10-16 15:13:15 +0900

    [unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = 0;
+    // 반복문 돌려 숫자인 경우에만 합하기 isNaN -> 숫자가 아닌 경우 true
+    my_string = my_string.split('');
+    for (let i = 0; i < my_string.length; i++) {
+        if (isNaN(my_string[i]) === false) {
+            answer += parseInt(my_string[i]);
+        }
+    }
+    return answer;
+}
