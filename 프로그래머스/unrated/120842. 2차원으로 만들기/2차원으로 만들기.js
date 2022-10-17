2b044f28a09f888461d5e34a5d7d4be3396bd046
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-10-17 22:43:13 +0900

    [unrated] Title: 2차원으로 만들기, Time: 3.50 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,5 +1,6 @@
 function solution(num_list, n) {
     const answer = [];
+    console.log(num_list.length)
     for(let i = 0; i < num_list.length; i + n) {
         answer.push(num_list.splice(i,i+n));
     }
