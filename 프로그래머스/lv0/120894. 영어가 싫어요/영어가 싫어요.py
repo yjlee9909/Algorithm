b0e07c3880467532dd4e837b50f73d2ab6075ca4
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:54:03 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.32 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,19 @@
+import re
+
+def solution(numbers):
+    s = ''
+    d = {
+        'zero' : '0',
+        'one' : '1',
+        'two' : '2',
+        'three' : '3',
+        'four' : '4',
+        'five' : '5',
+        'six' : '6',
+        'seven' : '7',
+        'eight' : '8',
+        'nine' : '9'
+    }
+    for i in re.findall(r'(zero|one|two|three|four|five|six|seven|eight|nine)', numbers):
+        s += d[i]
+    return int(s)
