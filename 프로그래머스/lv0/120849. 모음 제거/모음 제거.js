0f9578c4890e0098f8f86fe82137edf40710229d
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 21:44:24 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,3 +1,15 @@
 function solution(my_string) {
     return my_string.replace(/[aeiou]/g, '');
 }
+
+// 파이썬
+
+// sol.1
+// def solution(my_string):
+//     return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+
+// sol.2
+// import re
+
+// def solution(my_string):
+//     return re.sub(r'[aeiou]', '', my_string)
