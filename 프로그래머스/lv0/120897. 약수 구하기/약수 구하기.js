d2488b7c18edaed4d3330ccf473242cd2d09268e
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 20:49:52 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,27 @@
 function solution(n) {
-    const answer = [];
-    for (let i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            answer.push(i);
-        }
-    }
-    return answer;
+    // sol.1
+    // const answer = [];
+    // for (let i = 1; i <= n; i++) {
+    //     if (n % i == 0) {
+    //         answer.push(i);
+    //     }
+    // }
+    // return answer;
+    
+    // sol.2
+    return Array(n).fill().map((_, i) => i + 1).filter(v => n % v === 0);
 }
+
+// 파이썬
+
+// sol.1
+// def solution(n):
+//     약수 = []
+//     for i in range(1, n+1):
+//         if n % i == 0:
+//             약수.append(i)
+//     return 약수
+
+// sol.2
+// def solution(n):
+//     return [i for i in range(1, n+1) if n % i == 0]
