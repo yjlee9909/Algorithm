5fafe7cd5db9d173b9f84f846c3aff26fcd1a4d2
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 21:19:21 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,5 +1,8 @@
 function solution(n, numlist) {
     // filter -> 주어진 함수의 테스트를 통과하는 요소를 모아 새로운 배열로 반환
-    numlist = numlist.filter(i => i % n === 0);
-    return numlist;
+    return numlist.filter(i => i % n === 0);
 }
+
+// 파이썬
+// def solution(n, numlist):
+//     return [i for i in numlist if i % n == 0]
