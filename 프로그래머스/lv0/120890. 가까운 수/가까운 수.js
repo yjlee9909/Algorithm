f5d6c8248bfd394012ff7d85de66b967fa97f40f
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:52:19 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,9 +1,24 @@
+// function solution(array, n) {
+//     let obj = {};
+//     // 두 수의 차를 value로
+//     array.forEach(item => {
+//         obj[item] = Math.abs(item - n);
+//     })
+//     const minNum = Math.min(...Object.values(obj));
+//     return parseInt(Object.keys(obj).find(key => obj[key] === minNum));
+// }
+
+// sol.2
 function solution(array, n) {
-    let obj = {};
-    // 두 수의 차를 value로
-    array.forEach(item => {
-        obj[item] = Math.abs(item - n);
-    })
-    const minNum = Math.min(...Object.values(obj));
-    return parseInt(Object.keys(obj).find(key => obj[key] === minNum));
+    array.sort((a, b) => a - b);
+    let diff = Infinity;
+    let ind = Infinity;
+    for (let i in array) {
+        let calc = Math.abs(n - array[i]);
+        if (calc < diff) {
+            diff = calc;
+            ind = i;
+        }
+    }
+    return array[ind];
 }
