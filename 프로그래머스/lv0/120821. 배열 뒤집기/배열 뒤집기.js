72a6fa5a9a5860af8e3a42b5b1cca05d3ed193e5
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 20:37:20 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    // sol.1
+    // var answer = [];
+    // for (let i = num_list.length-1; i >= 0; i--) {
+    //     answer.push(num_list[i]);
+    // }
+    // return answer;
+    
+    // sol.2
+    return num_list.reverse();
+}
+
+// 파이썬
