99ca50579616a01621a39772ed29e385a9c8c3ec
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 19:55:28 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,26 @@
+function solution(n) {
+    // sol.1
+    // let answer = 0;
+    // let sum = 0;
+    // answer = n.toString().split('');
+    // for(let i = 0; i < answer.length; i++) {
+    //     sum += parseInt(answer[i],10);
+    // }
+    // return sum;
+    
+    // sol.2
+    return n.toString().split('').map(v => +v).reduce((acc, cur) => acc + cur);
+    
+}
+
+// 파이썬
+// sol.1
+// def solution(n):
+//     return sum(map(int, str(n)))
+
+// sol.2
+// def solution(n):
+//     s = 0
+//     for i in str(n):
+//         s += int(i)
+//     return s
