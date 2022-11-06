b6429264973a820d0303817dba1aba90e20cf36a
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 20:15:26 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,17 @@
+function solution(my_string) {
+    // var answer = my_string.split('').reverse().join('');
+    
+    // sol.1
+    // let str = '';
+    // for (let i = my_string.length-1; i >= 0; i--) {
+    //     str += my_string[i];
+    // }
+    // return str;
+    
+    // sol.2
+    return my_string.split('').reverse().join('')
+}
+
+// 파이썬
+// def solution(my_string):
+//     return my_string[::-1]
