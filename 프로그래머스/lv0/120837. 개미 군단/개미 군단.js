3addfaf9d426e47450615059aa7acdd90f5da452
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:14:06 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,10 @@
 function solution(hp) {
-    const 장군개미수 = parseInt(hp / 5);
-    const 병정개미수 = parseInt((hp % 5) / 3);
-    const 일개미수 = parseInt(((hp % 5) % 3) / 1);
-    return 장군개미수 + 병정개미수 + 일개미수;
+    // const 장군개미수 = parseInt(hp / 5);
+    // const 병정개미수 = parseInt((hp % 5) / 3);
+    // const 일개미수 = parseInt(((hp % 5) % 3) / 1);
+    // return 장군개미수 + 병정개미수 + 일개미수;
+    
+    // sol.32
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + ((hp % 5) % 3);
+
 }
