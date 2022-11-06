14bd3437c2179b7ef93596f8d8b1c23d8331ea9b
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 21:16:26 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,7 +1,23 @@
 function solution(numbers, num1, num2) {
-    return numbers.slice(num1, num2+1);
+    // sol.1
+    // return numbers.slice(num1, num2+1);
+    
+    // sol.2
+    return numbers.splice(num1, num2 - num1 + 1);
 }
 
 // 파이썬
 // def solution(numbers, num1, num2):
 //     return numbers[num1:num2+1]
+
+// splice
+// let test = [1, 2, 3, 4, 5, 6, 7]
+// test.splice(2, 4, [10, 20])
+// (4) [3, 4, 5, 6]
+// test
+// (4) [1, 2, Array(2), 7]0: 11: 22: (2) [10, 20]3: 7length: 4[[Prototype]]: Array(0)
+// let test = [1, 2, 3, 4, 5, 6, 7]
+// test.splice(2, 4, 10, 20)
+// (4) [3, 4, 5, 6]
+// test
+// (5) [1, 2, 10, 20, 7]
