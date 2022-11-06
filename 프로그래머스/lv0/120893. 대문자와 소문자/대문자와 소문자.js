4ae7cfac81088a87d4f49af9282d5162d4cfa8c8
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:23:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,19 @@
+// function solution(my_string) {
+//     let answer = '';
+//     for (let i of my_string) {
+//         if (i === i.toUpperCase()) {
+//             answer += i.toLowerCase();
+//         } else {
+//             answer += i.toUpperCase();
+//         }
+//     } 
+//     return answer;
+// }
+
+// sol.2
 function solution(my_string) {
-    let answer = '';
-    for (let i of my_string) {
-        if (i === i.toUpperCase()) {
-            answer += i.toLowerCase();
-        } else {
-            answer += i.toUpperCase();
-        }
-    } 
+    var answer = "";
+    for (let c of my_string)
+        answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
     return answer;
 }
