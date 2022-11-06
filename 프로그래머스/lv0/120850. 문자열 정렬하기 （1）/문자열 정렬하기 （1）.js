29c7259549b10a258ac0f6345805331db3829fa2
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:16:12 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,6 +1,12 @@
 function solution(my_string) {
     // 정규식으로 문자 제거
-    my_string = my_string.replace(/[^0-9]/g, "");
-    my_string = [...my_string].map((i) => parseInt(i)).sort((a ,b) => a - b);
-    return my_string;
+    // my_string = my_string.replace(/[^0-9]/g, "");
+    // my_string = [...my_string].map((i) => parseInt(i)).sort((a ,b) => a - b);
+    // return my_string;
+    
+    // sol.2
+        return my_string
+        .match(/\d/g)
+        .sort((a, b) => a - b)
+        .map((n) => Number(n));
 }
