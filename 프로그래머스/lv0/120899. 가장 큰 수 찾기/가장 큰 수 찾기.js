a4e9c134735a2643435b088a2c5d88b15dd68983
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 20:40:37 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,3 +1,7 @@
 function solution(array) {
     return [Math.max(...array), array.indexOf(Math.max(...array))];
 }
+
+// 파이썬
+// def solution(array):
+//     return [max(array), array.index(max(array))]
