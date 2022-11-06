c0cb38de8aa1f4ef4d080f1d775b95847333228e
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 21:42:03 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.77 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,10 +1,41 @@
 function solution(n) {
-    const answer = [];
-    // 반복문 돌면서 나머지가 0
-    for (let i = 1; i <= n; i++) {
-        if(n % i === 0) {
-            answer.push([i, parseInt(n / i)]);
+    // sol.1
+    // const answer = [];
+    // // 반복문 돌면서 나머지가 0
+    // for (let i = 1; i <= n; i++) {
+    //     if(n % i === 0) {
+    //         answer.push([i, parseInt(n / i)]);
+    //     }
+    // } 
+    // return answer.length;
+    
+    // sol.2
+    var answer = 0;
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) {
+            answer += 1;
         }
-    } 
-    return answer.length;
+    }
+    return answer;
+
 }
+
+// 파이썬
+// sol.1
+// 시간초과 코드
+// def solution(n):
+//     answer = []
+//     for i in range(1, n+1):
+//         for j in range(1, n+1):
+//             if i * j == n:
+//                 answer.append([i, j])
+//     return len(answer)
+
+
+// sol.2
+// def solution(n):
+//     answer = 0
+//     for i in range(n):
+//         if n % (i+1) == 0:
+//             answer += 1
+//     return answer
