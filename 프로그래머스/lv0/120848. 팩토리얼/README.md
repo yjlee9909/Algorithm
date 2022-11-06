c0b52f2e48dfd8b4d972d356ab93e7d24aa6007d
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-06 22:49:39 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/README.md
+++ 프로그래머스/lv0/120848. 팩토리얼/README.md
@@ -1,10 +1,10 @@
 # [level 0] 팩토리얼 - 120848 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.03 ms
+메모리: 33.5 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,9 +1,18 @@
+// function solution(n) {
+//     let result = 1;
+//     let maxI = 1;
+//     for (maxI = 1; maxI <= n; maxI++) {
+//         result *= maxI;
+//         if (result > n) return maxI - 1;
+//     }
+//     return maxI - 1;
+// }
+
+// sol.2
+
 function solution(n) {
-    let result = 1;
-    let maxI = 1;
-    for (maxI = 1; maxI <= n; maxI++) {
-        result *= maxI;
-        if (result > n) return maxI - 1;
-    }
-    return maxI - 1;
+    let i = 1;
+    let f = 1;
+    while (f * i < n) f *= ++i;
+    return i;
 }
