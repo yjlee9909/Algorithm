6e8f5a0fc20967ad89269961e297d72b43a39466
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-11-08 17:45:15 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,19 @@
+//sol.1
+// function solution(absolutes, signs) {
+//     let arr = [];
+//     for (let i = 0; i < absolutes.length; i++) {
+//         if (signs[i] === true) {
+//             arr.push(absolutes[i]);
+//         } else {
+//             arr.push(absolutes[i] * -1);
+//         }
+//     }
+//     return arr.reduce((acc, cur) => acc + cur);
+// }
+
+// sol.2
+
 function solution(absolutes, signs) {
-    let arr = [];
-    for (let i = 0; i < absolutes.length; i++) {
-        if (signs[i] === true) {
-            arr.push(absolutes[i]);
-        } else {
-            arr.push(absolutes[i] * -1);
-        }
-    }
-    return arr.reduce((acc, cur) => acc + cur);
+    return absolutes.reduce((acc, cur, i) => acc + cur * (signs[i] ? 1 : -1),0);
 }
+
