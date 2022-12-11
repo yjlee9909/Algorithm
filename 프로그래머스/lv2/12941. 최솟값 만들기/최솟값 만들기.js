a14e87754cec88c67b10895f93eaef8754a9b382
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-12-11 17:31:17 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,9 @@
+function solution(A,B){
+    A = A.sort((a,b) => a - b);
+    B = B.sort((a,b) => b - a);
+    let sum = 0;
+    for (let i = 0; i < A.length; i++) {
+        sum += A[i] * B[i]
+    }
+    return sum
+}
