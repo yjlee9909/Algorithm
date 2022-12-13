15e952bea256d0cc4c20478f44d256f1bfe90389
yoojin <63508955+yjlee9909@users.noreply.github.com>
2022-12-13 21:06:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    let count = 0;
+    while (num !== 1 && count !== 500) {
+        if (num % 2 === 0) {
+            num = num / 2;
+        } else {
+            num = num * 3 + 1;
+        }
+        count++;
+    }
+    return num === 1 ? count : -1;
+}
