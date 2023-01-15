750cff981efc9c2c250a579dcb3cac7f62163c0b
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-01-15 17:06:30 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -3,6 +3,5 @@ function solution(id_pw, db) {
   db.map((item, i) => {if(id_pw[0] === item[0] && id_pw[1] === item[1]) result = "login"
 	else if(id_pw[0] === item[0] && id_pw[1] !== item[1]) result =  "wrong pw"
 	else if(id_pw[0] !== item[0] && id_pw[1] !== item[1]) result = "fail"})
-	
-  return result
+  return result;
 }
