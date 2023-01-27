875645a029e8be71dece4346ca8b7338a5135cc7
yjlee9909 <yjlee9909@naver.com>
2023-01-27 17:10:36 +0900

    10일 챌린지 data types

--- hackerrank/challengd10Days/Day0.js
+++ hackerrank/challengd10Days/Day0.js
@@ -1,6 +1,18 @@
+// Day 0 : Hello, World
 function greeting(parameterVariable) {
   // This line prints 'Hello, World!' to the console:
   console.log("Hello, World!");
   console.log(parameterVariable);
   // Write a line of code that prints parameterVariable to stdout using console.log:
+}
+
+// Day 0 : Data Types
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  const firstDecimal = 4.0;
+  const firstString = "HackerRank ";
+
+  console.log(+firstInteger + +secondInteger);
+  console.log(+firstDecimal + +secondDecimal);
+  console.log(firstString + secondString);
 }
