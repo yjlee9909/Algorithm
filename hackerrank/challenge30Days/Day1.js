d641d0468ec1b776a942b1f699acdc35b32bf1eb
yjlee9909 <yjlee9909@naver.com>
2023-01-29 17:48:22 +0900

    30일 챌린지 Data Types

---
+++ hackerrank/challenge30Days/Day1.js
@@ -0,0 +1,9 @@
+// Day 1: Data Types
+function main() {
+  var i = 4;
+  var d = 4.0;
+  var s = "HackerRank ";
+  console.log(+i + +input_stdin_array[0]);
+  console.log((parseFloat(d) + parseFloat(input_stdin_array[1])).toFixed(1));
+  console.log(s + input_stdin_array[2]);
+}
