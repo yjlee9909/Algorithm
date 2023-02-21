6aa1df392008a335ed4004c1399661b9a4ca09ae
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-02-21 12:43:07 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,13 +1,8 @@
-let array = [1, 5, 2, 6, 3, 7, 4];
-let commands = [
-    [2, 5, 3],
-    [4, 4, 1],
-    [1, 7, 3],
-];
-
 function solution(array, commands) {
-    return commands.map(
-        ([from, to, k]) =>
-            array.slice(from - 1, to).sort((a, b) => a - b)[k - 1],
-    );
+    let answer = [];
+    for (let i = 0; i < commands.length; i++) {
+        let arr = array.slice(commands[i][0] - 1, commands[i][1]).sort((a,b) => a - b)
+        answer.push(arr[commands[i][2] - 1]);
+    }
+    return answer;
 }
