79c2d36a33ccfd1c37f5238481481fb24453c864
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-02-23 14:16:28 +0900

    [level 1] Title: 직사각형 별찍기, Time: 78.29 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,7 +2,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-
     let sum = '';
     for (let i = 1; i <= b; i++) {
         for (let j = 1; j <= a; j++) {
@@ -12,3 +11,4 @@ process.stdin.on('data', data => {
     }
     console.log(sum)
 });
+
