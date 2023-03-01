c3d18500f0278d19acef7cca2864cdd7aac76193
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-03-01 15:33:51 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const num = parseInt(input[0]);
+
+for (let i = 1; i <= num; i++) {
+  console.log(
+    `Case #${i}: ${parseInt(input[i].split(" ")[0])} + ${parseInt(
+      input[i].split(" ")[1]
+    )} = ${parseInt(input[i].split(" ")[0]) + parseInt(input[i].split(" ")[1])}`
+  );
+}
