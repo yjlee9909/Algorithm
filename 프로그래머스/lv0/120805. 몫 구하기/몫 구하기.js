a7ecf29f04a7b8838eb37d8a1baa9677bbacd328
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-03-10 17:07:57 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -1,7 +1,3 @@
 function solution(num1, num2) {
-    var answer = Math.floor(num1 / num2);
-    return answer;
+    return ~~(num1 / num2);
 }
-// 파이썬 코드
-// def solution(num1, num2):
-//     return num1 // num2
