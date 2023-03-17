df96caaac2c96b2b4fc37e3f61720c5b9b2ef433
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-03-17 17:43:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let str = input[0].charCodeAt();
+console.log(str);
