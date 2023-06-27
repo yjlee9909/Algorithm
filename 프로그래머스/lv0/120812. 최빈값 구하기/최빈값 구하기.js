67debe8f0021ba4a607504cde5d575f4b5f6dd81
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-06-27 15:17:06 +0900

    [level 0] Title: 최빈값 구하기, Time: 4.30 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -11,19 +11,6 @@ function solution(array) {
     if (countValue[0] === countValue[1]) return -1;
     for (const el of objConvert) {
         console.log(el)
-        if (el[1] === countValue[0]) {
-            return parseInt(el[0]);
-        } 
+        if (el[1] === countValue[0]) return parseInt(el[0]);
     }
-    
-//     countValue.sort((a,b) => b - a);
-    
-//     if (countValue.length === 1) {
-//         return countValue[0];
-//     } else if (countValue[0] === countValue[1]) {
-//         return -1;
-//     } else {
-//         return Math.max(...Object.values(maxNum));
-//     }
-
 }
