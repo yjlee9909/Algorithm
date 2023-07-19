739b1dd93d26c22d9e4bdfee923e8c7e61c67e9d
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-07-19 17:53:42 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+console.log(Math.pow(2 ** +input + 1, 2));
