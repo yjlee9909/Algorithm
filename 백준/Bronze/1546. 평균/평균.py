7657522fc810580c928abdd90454836585ae0aeb
yoojin <63508955+yjlee9909@users.noreply.github.com>
2023-12-14 23:48:08 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,12 +1,5 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-arr = list(map(int,input().split(' ')))
-M = max(arr)
-new_arr = []
-sum = 0
-for i in arr:
-    new_arr.append(i/M*100)
-for i in new_arr:
-    sum += i
-print(sum/N)
+N = input()
+numbers = list(map(int,input().split()))
+maxNum = max(numbers)
+sum = sum(numbers)
+print(sum * 100 / maxNum / int(N))
